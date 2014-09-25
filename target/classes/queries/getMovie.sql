77743c5d7c92d48bf23a801c0162cf2aa84cfca7
Manikandan <rkmanikarthick@gmail.com>
2014-09-25 22:30:41 +0530

    Removing target folder

--- target/classes/queries/getMovie.sql
+++
@@ -1,9 +0,0 @@
-select
-    movie_id AS movieId,
-    movie_name AS movieName,
-    song_id AS song$songId,
-    song_name AS song$songName
-from movie
-left join song
-using ( movie_id )
-where movie_id = :movieId
